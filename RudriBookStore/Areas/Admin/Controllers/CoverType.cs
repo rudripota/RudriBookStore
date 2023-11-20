546cfb062088137e020c64d94284b2bbac9375d6
W0817379 <W0817379@myscc.ca>
2023-11-20 12:15:02 -0500

    Added CoverType class, interface and controller for MVC in the folder controller

---
+++ RudriBookStore/Areas/Admin/Controllers/CoverType.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RudriBookStore.Areas.Admin.Controllers
+{
+    public class CoverType : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RudriBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RudriBooks.DataAccess.Repository
+{
+    class CoverTypeRepository
+    {
+    }
+}
