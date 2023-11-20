cf21ee5e5ea3eae2b1126ec8a0819b6737b9f93c
Rudri Ravi Pota <W0817379@myscc.ca>
2023-11-20 16:45:23 -0500

    made a covertype javascript file in www.root and added new code to product.cs

---
+++ RudriBooks.Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RudriBooks.Models
+{ 
+    public class Product
+    { 
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ISBN { get; set; }
+    public string Author { get; set; }
+    [Required]
+    [Range(1, 10000)]
+    public double ListPrice { get; set; }
+    public string ImageUrl{ get; set; }
+
+    [Required]
+    public int CategoryId { get; set; } // foreign key reference
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+
+    [Required]
+    public int CoverTypeId { get; set; }
+    [ForeignKey("CategoryId")] 
+    public CoverType Covertype { get; set; } // foreign key reference
+    }
+}
